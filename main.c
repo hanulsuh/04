facc925ffa05f39e1a29f5694676af126f6f5ad6
서한울 <hwsuh06@gmail.com>
2025-09-25 16:19:51 +0900

    06

--- main.c
+++ main.c
@@ -2,19 +2,17 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-	unsigned int x;
-	int b;
+	int sec;
 	
-	printf("Input a number : ");
-	scanf("%ui", &x);
+	printf("Input the second : ");
+	scanf("%i", &sec);
 	
-	for (b=0; x!=0; x>>=1) {
-		if (x&1) {
-			b++;
-		}
-	}
-	
-	printf("The result is : %i\n", b);
+	printf("The time for %i second is %i : %i : %i\n", 
+						sec,
+						sec/3600,
+						(sec%3600)/60,
+						sec%60
+						);
 
 	return 0;
 }
